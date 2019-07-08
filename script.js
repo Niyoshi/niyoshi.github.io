690679e6495cfc2f0d33e48f7916fb31301542af
niyoshi <niyoshi@gmail.com>
2019-07-08 19:40:30 -0400

    changes from glitch

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 
 // prints "hi" in the browser's dev tools console
 console.log('hi');
+
+$('.flex-container').scroll(function(event) {
+  console.log($('#scroll-checker').offset());
+   if ($('#scroll-checker').offset().top <= 250) {
+    $('#year').text($('.item-3').data('message'));
+  } 
+  
+  else if($('#scroll-checker').offset().top <= 750){
+    $('#year').text($('.item-2').data('message'));
+  }
+  
+  else {
+    $('#year').text('2019');
+  }
+});
